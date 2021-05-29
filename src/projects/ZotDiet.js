4d3bae1db37c8f0f550c9bbb2dafb89de8f2c3b4
Amy Weitzman <amyweitzman@yahoo.com>
2021-05-29 14:58:39 -0700

    update projects

---
+++ src/projects/ZotDiet.js
@@ -0,0 +1,11 @@
+export const name = "Zot Diet";
+
+export const longText = "In my Next Generation Search Systems course, I worked in a group of 4 to design and implement a personalized meal recommendation web application using React.js and Firebase. We integrated the Spoonacular API to generate meal plans based on the user's personal profile, allergies, and food preferences. One of my tasks was developing an algorithm for the meal liking/disliking system to allow users to provide feedback and receive more customized meal recommendations. I also incorporated natural language processing techniques to standardize data and make the meal ranking algorithm more efficient. Overall, as a team, we were able to create a viable product over the course of 10 weeks.";
+
+export const shortText = "In my Next Generation Search Systems course, I worked in a group of 4 to design and implement a personalized meal recommendation web application...";
+
+export const dates = "Winter 2021";
+
+export const techUsed = "React.js, HTML, CSS, Firebase, REST API";
+
+export const link = "";
